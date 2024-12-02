f34e7b215422cf4f2cd14ba2cfc57a4d2a61e746
XchrisGG <christianale3103@gmail.com>
2024-12-01 18:58:43 -0600

    Se crea funcion para agregar las rutas para la navegación

--- app/src/main/java/com/mexware/stresswatch_/MainActivity.kt
+++ app/src/main/java/com/mexware/stresswatch_/MainActivity.kt
@@ -9,8 +9,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mexware.stresswatch_.layouts.ChatScreen
 import com.mexware.stresswatch_.layouts.FormLogin
 import com.mexware.stresswatch_.layouts.HomeScreen
 import com.mexware.stresswatch_.ui.theme.StressWatch_Theme
@@ -20,7 +25,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            HomeScreen()
+            val navController = rememberNavController()
+
+            NavHost(navController = navController, startDestination = "home"){
+                composable(route = "home"){ HomeScreen() }
+                composable(route = "Chat"){ ChatScreen() }
+            }
 
         }
     }
