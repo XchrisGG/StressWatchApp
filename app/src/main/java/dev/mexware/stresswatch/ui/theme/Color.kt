6ad4cc2d8344fa695b77524df5e86396d93ce9ad
XchrisGG <christianale3103@gmail.com>
2025-08-12 17:54:27 -0600

    Se configura archivo color.kt

--- app/src/main/java/dev/mexware/stresswatch/ui/theme/Color.kt
+++ app/src/main/java/dev/mexware/stresswatch/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package dev.mexware.stresswatch.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val StressDarkBlue   = Color(0xFF011E3D) // Fondo principal
+val StressMint       = Color(0xFF60B9A1) // Botones y acentos
+val StressBlack      = Color(0xFF030303) // Texto oscuro
+val StressWhite      = Color(0xFFFFFFFF) // Texto claro
+val StressGrayLight  = Color(0xFFF5F5F5) // Fondos claros
+val StressBlackPure  = Color(0xFF000000) // Negro puro
